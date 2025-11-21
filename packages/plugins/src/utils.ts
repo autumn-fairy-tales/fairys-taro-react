91655c08983e15f9af52672395befd5c288a569a
SunLxy <1011771396@qq.com>
2025-11-21 17:37:22 +0800

    feat: 优化替换

--- packages/plugins/src/utils.ts
+++ packages/plugins/src/utils.ts
@@ -1,17 +1,20 @@
+import { transformSelector, transformEscapESelector, defaultRules } from 'unplugin-transform-class/utils';
+
 /**
  * 处理特殊字符正则表达式
  */
-export const replaceRegx = [
-  [':', '__mh__'],
-  ['\\/', '__xg__'],
-  ['\\[', '__zkh1__'],
-  ['\\]', '__zhk2__'],
-  ['\\(', '__xkh1__'],
-  ['\\)', '__xkh2__'],
-  ['\\!', '__gqh__'],
-  ['%', '__bfh__'],
-  ['\\*', '__x__'],
-];
+export const defaultReplaceRules = {
+  ...defaultRules,
+  ':': '__mh__',
+  '/': '__xg__',
+  '[': '__zkh1__',
+  ']': '__zhk2__',
+  '(': '__xkh1__',
+  ')': '__xkh2__',
+  '!': '__gqh__',
+  '%': '__bfh__',
+  '*': '__x__',
+} as const;
 
 export interface ReplaceOptions {
   /**内容*/
@@ -21,21 +24,18 @@ export interface ReplaceOptions {
   /**前缀*/
   prefix?: string;
   /**替换规则*/
-  regexList?: Array<[string, string]>;
+  replaceRules?: Record<string, string>;
 }
 
 export const replace = (options: ReplaceOptions) => {
-  const { content, isPostcss = true, prefix = 'fairystaro--', regexList = replaceRegx } = options;
+  const { content, isPostcss = true, prefix = 'fairystaro--', replaceRules = defaultReplaceRules } = options;
   // /fairystaro--(\S+?)(?=[\s'"]|$)/g
   return content.replace(new RegExp(`${prefix}(\\S+?)(?=[\\s'"]|$)`, 'g'), (match) => {
     let _newStr = match;
-    for (let index = 0; index < regexList.length; index++) {
-      const item = regexList[index];
-      if (isPostcss) {
-        _newStr = _newStr.replace(new RegExp(`\\\\${item[0]}`, 'g'), item[1]);
-      } else {
-        _newStr = _newStr.replace(new RegExp(`${item[0]}`, 'g'), item[1]);
-      }
+    if (isPostcss) {
+      _newStr = transformEscapESelector(_newStr, replaceRules);
+    } else {
+      _newStr = transformSelector(_newStr, replaceRules);
     }
     return _newStr;
   });
