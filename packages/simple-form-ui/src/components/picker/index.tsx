797d46b65ffadac880b4ecaa51801d15118f4115
SunLxy <1011771396@qq.com>
2026-01-24 21:19:59 +0800

    feat: 修改 Picker 选择存储数据

--- packages/simple-form-ui/src/components/picker/index.tsx
+++ packages/simple-form-ui/src/components/picker/index.tsx
@@ -1,22 +1,37 @@
 import { View } from '@tarojs/components';
-import { Picker, TaroPickerProps, PickerOptions } from '@nutui/nutui-react-taro';
+import { Picker, TaroPickerProps, PickerOptions, PickerOption } from '@nutui/nutui-react-taro';
 import { useMemo, useState } from 'react';
 import { FairysTaroTextClearBase } from 'components/clear';
 
 export interface FairysTaroPickerProps extends Omit<Partial<TaroPickerProps>, 'value' | 'onChange'> {
   placeholder?: string;
-  value?: PickerOptions;
-  onChange?: (value: PickerOptions) => void;
+  value?: PickerOptions | PickerOption;
+  onChange?: (value: PickerOptions | PickerOption) => void;
   bodyClassName?: string;
   bodyStyle?: React.CSSProperties;
 }
 
 export const FairysTaroPickerBase = (props: FairysTaroPickerProps) => {
-  const { placeholder = '请选择', bodyClassName, bodyStyle, className, style, value, onChange, ...rest } = props;
-
+  const {
+    placeholder = '请选择',
+    bodyClassName,
+    bodyStyle,
+    className,
+    style,
+    value,
+    onChange,
+    options,
+    ...rest
+  } = props;
   const [visible, setVisible] = useState(false);
 
+  // 根据 options 长度进行判断是否为单层级, 单层的value存储对象，多层的存储数组
+  const isSingle = (options || []).length <= 1;
+
   const _renderValue = useMemo(() => {
+    if (isSingle) {
+      return (value as PickerOption)?.label || '';
+    }
     if (Array.isArray(value)) {
       return value.map((item) => item.label).join(' / ');
     }
@@ -24,6 +39,13 @@ export const FairysTaroPickerBase = (props: FairysTaroPickerProps) => {
   }, [value]);
 
   const _value = useMemo(() => {
+    if (isSingle) {
+      const _value = (value as PickerOption)?.value || '';
+      if (typeof _value === 'number') {
+        return [_value];
+      }
+      return [_value].filter(Boolean);
+    }
     if (Array.isArray(value)) {
       return value.map((item) => item.value);
     }
@@ -42,6 +64,7 @@ export const FairysTaroPickerBase = (props: FairysTaroPickerProps) => {
       </FairysTaroTextClearBase>
       <Picker
         {...rest}
+        options={options}
         className={`fairys-taro-picker-body fairystaroform__text-left ${bodyClassName || ''}`}
         style={bodyStyle}
         value={_value}
@@ -49,7 +72,11 @@ export const FairysTaroPickerBase = (props: FairysTaroPickerProps) => {
         onClose={() => setVisible(false)}
         onConfirm={(selectedOptions) => {
           setVisible(false);
-          onChange?.(selectedOptions);
+          if (isSingle) {
+            onChange?.(selectedOptions?.[0]);
+          } else {
+            onChange?.(selectedOptions);
+          }
         }}
       />
     </View>
